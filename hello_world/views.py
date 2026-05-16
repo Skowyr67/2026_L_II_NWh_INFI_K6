d59dd67c7c603eec4ad3c9eb6af095e12d3f7dc0
admstud <admstud@MX1-Docker-20.EDU-WSB.local>
2026-05-16 15:43:38 +0200

    fix1

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,19 +1,19 @@
-from hello_world import app
-from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
-msg = "Hello World!"
+from hello_world import app
+from hello_world.formater import PLAIN, SUPPORTED, get_formatted
 
-@app.route('/')
+
+MOJE_IMIE = "Michal "
+MSG = "Hello World!"
+
+
+@app.route("/")
 def index():
-    output = request.args.get('output')
-    if not output:
-        output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    output = request.args.get("output", PLAIN)
+    return get_formatted(MSG, MOJE_IMIE, output.lower())
 
-@app.route('/outputs')
+
+@app.route("/outputs")
 def supported_output():
     return ", ".join(SUPPORTED)
